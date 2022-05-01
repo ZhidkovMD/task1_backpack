4f5c28813c0c7501e688110568fc82bbd236c98c
ZhidkovMD <103329426+ZhidkovMD@users.noreply.github.com>
2022-05-01 18:24:12 +0300

    Add files via upload

--- backpack.cpp
+++ backpack.cpp
@@ -1,19 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
 using namespace std;
-int max(int a, int b){
-    return (a > b) ? a : b;
+namespace fs = filesystem;
+
+int max(int x, int y){
+    return (x > y) ? x : y;
 }
-int knapSack(int W, int wt[], int val[], int n){
-    if(n == 0 || W == 0)  return 0;
-    if(wt[n - 1] > W)  return knapSack(W, wt, val, n - 1);
-    else  return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1));
+
+int backpack(int W, vector<int> wt, vector<int> val, int n){
+    vector<int> dp(W+1);
+    fill(dp.begin(), dp.end(), 0);
+    for(int i = 1; i < n + 1; i++){
+        for(int w = W; w >= 0; w--){
+            if(wt[i - 1] <= w){
+                dp[w] = max(dp[w], dp[w - wt[i - 1]] + val[i - 1]);
+            }
+        }
+    }
+    return dp[W];
 }
-// ����
+
 int main(){
-    int val[] = { 60, 100, 120 };
-    int wt[] = { 10, 20, 30 };
-    int W = 50;
-    int n = sizeof(val) / sizeof(val[0]);
-    cout << knapSack(W, wt, val, n);
+    string path = "/Users/Михаил/FSR/алгоритмы&прога/4й семестр/backpack";
+          auto it = fs::directory_iterator(path);
+          vector<fs::path> array_path;
+          copy_if(fs::begin(it), fs::end(it), std::back_inserter(array_path),
+              [](const auto& entry) {
+                  return fs::is_regular_file(entry);
+          });
+    for (auto& p : array_path) {
+        ifstream ff;
+        ff.open(p.string());
+        cout << p.string() << endl;
+        int N, W;
+        ff >> n >> W;
+        vector<int> vals;
+        vector<int> ws;
+        for (int i = 0; i < N; i++) {
+            int val, wt;
+            ff >> val >> wt;
+            vals.push_back(val);
+            ws.push_back(wt);
+        }
+        int maxvalue = backpack(W, ws, vals, N);
+        cout << maxvalue << endl;
+    }
     return 0;
 }
